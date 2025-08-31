179a5eddbf20a8021223a829f2332c151df257d3
bibekkumarborah007 <bibekkumarborah@gmail.com>
2025-09-01 03:55:08 +0530

    update

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -23,11 +23,3 @@ function App() {
 }
 
 export default App
-
-//props: makes component reusable: when a cart made keep it in component and keep resuse it.
-//Generally we keep html, css, js in specifically based on tech. 
-// Reacts says instead of segregating based on technologies but segregate on the basis of what it work and what written inside. eg. like a card in order to reuse it put all html,css,js inside it so that when we pass a value inside it the card changes. 
-
-
-
-//props: value can be send from one component to another with porps access eg card prop pass here and handle it in component
--- READ.md
+++ READ.md
@@ -1,2 +1,7 @@
 ## repo
 (React Git Repo) https://github.com/facebook/react
+
+#hooks
+: https://github.com/facebook/react/tree/main/packages/react/src
+: https://github.com/facebook/react/blob/main/packages/react/src/ReactHooks.js
+
