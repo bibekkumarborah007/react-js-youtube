cf05d80855b5675858078aba03c1542854931acd
bibekkumarborah007 <bibekkumarborah@gmail.com>
2025-08-31 13:34:22 +0530

    update

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,6 +1,6 @@
 function customRender(reactElement, container){
     /*const domElement = document.createElement
-    (reactElement.type)
+    (reactElement.type) //empty element gotcreated
     domElement.innerHTML = reactElement.children
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
@@ -20,12 +20,12 @@ const reactElement = {
     type: 'a',
     props: {
         href: 'https://google.com',
-        target: '_black'
+        target: '_blank'
     },
     children: 'Click me to visit google'
-}
+} //react uder the hood. lib use must use and write in such way and add this element in root
 
 const mainContainer = document.querySelector('#root')
 
 //render
-customRender(reactElement, mainContainer)//inject reacte lemtn and where?
+customRender(reactElement, mainContainer)//inject react in main
